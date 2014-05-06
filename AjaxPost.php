06dcde5bc399ff8f8484de9aacb8e8cbcefa05d1
Rangoly Jain <hirangoly@gmail.com>
2014-05-05 17:21:08 -0700

    Create AjaxPost.php

---
+++ AjaxPost.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">
+</script>
+<script>
+$(document).ready(function(){
+  $("button").click(function(){
+    $.post("demo_test_post.php",
+    {
+      name:"Donald Duck",
+      town:"Duckburg"
+    },
+    function(data,status){
+      alert("Data: " + data + "\nStatus: " + status);
+    });
+  });
+});
+</script>
+</head>
+<body>
+
+<button>Send an HTTP POST request to a page and get the result back</button>
+
+</body>
+</html>
